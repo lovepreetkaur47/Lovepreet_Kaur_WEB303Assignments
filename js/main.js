848057c7bbe0d4fa7073d2a892d2ba58f3137e21
w0832389 <W0824592@myscc.ca>
2023-09-24 22:54:34 -0400

    final code

--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
-// WEB303 Assignment 2
+$(document).ready(function () {
+    // Click event handler for the links
+    $('#prospect').click(function (event) {
+        event.preventDefault(); // Prevent the default link behavior
+        loadContent('prospect.html'); // Call the function to load 'prospect.html'
+    });
 
+    $('#convert').click(function (event) {
+        event.preventDefault(); // Prevent the default link behavior
+        loadContent('convert.html'); // Call the function to load 'convert.html'
+    });
 
+    $('#retain').click(function (event) {
+        event.preventDefault(); // Prevent the default link behavior
+        loadContent('retain.html'); // Call the function to load 'retain.html'
+    });
+
+    // Function to load content and animate
+    function loadContent(url) {
+        $('#content').fadeOut(400, function () {
+            $(this).empty(); // Clear existing content
+
+            // Load new content using .load() method
+            $(this).load(url, function () {
+                $(this).fadeIn(400); // Display the new content with fade-in animation
+            });
+        });
+    }
+});
