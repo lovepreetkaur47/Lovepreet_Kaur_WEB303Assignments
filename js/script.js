38294678d27500bce7ae52aef98dcdb63ac493d7
w0832389 <W0824592@myscc.ca>
2023-10-01 21:49:10 -0400

    update script

--- js/script.js
+++ js/script.js
@@ -1,51 +1,61 @@
+// Method for fetching data using $.getJSON
+function fetchDataWithGetJSON() {
+  $.getJSON('team.json', function(data) {
+    // Loop through the data array and display it
+    $.each(data, function(index, member) {
+      // Create HTML elements for each team member
+      var memberDiv = $('<div>');
+      var nameHeader = $('<h2>').text(member.name);
+      var positionHeader = $('<h5>').text(member.position);
+      var bioParagraph = $('<p>').text(member.bio);
 
-// Method for $.getjson request
-function getDataWithGetJSON() {
-  $.getJSON("team.json", function(data) {
-    $.each(data, function(index, item) {
-      var name = item.name;
-      var position = item.position;
-      var bio = item.bio;
-
-      var teamDiv = $("<div>").appendTo("#team");
-      $("<h2>").text(name).appendTo(teamDiv);
-      $("<h5>").text(position).appendTo(teamDiv);
-      $("<p>").text(bio).appendTo(teamDiv);
+      // Append the elements to the team div
+      memberDiv.append(nameHeader, positionHeader, bioParagraph);
+      $('#team').append(memberDiv);
     });
   });
+} 
+
+// Method for fetching data using $.ajax
+function fetchDataWithAjax() {
+  // Display "Loading..." message
+  $('#team').text('Loading...');
+
+  // Add a 3-second delay before making the AJAX request
+  setTimeout(function() {
+    $.ajax({
+      type: 'GET',
+      url: 'team.json',
+      dataType: 'json',
+      success: function(data) {
+        // Remove "Loading..." message
+        $('#team').empty();
+
+        // Loop through the data array and display it
+        $.each(data, function(index, member) {
+          // Create HTML elements for each team member
+          var memberDiv = $('<div>');
+          var nameHeader = $('<h2>').text(member.name);
+          var positionHeader = $('<h5>').text(member.position);
+          var bioParagraph = $('<p>').text(member.bio);
+
+          // Append the elements to the team div
+          memberDiv.append(nameHeader, positionHeader, bioParagraph);
+          $('#team').append(memberDiv);
+        });
+      },
+      error: function() {
+        // Display error message if AJAX request fails
+        $('#team').text('Error: Content could not be retrieved.');
+      }
+    });
+  }, 3000); // 3-second delay (3000 milliseconds)
 }
 
-// Method for $.ajax request
-function getDataWithAjax() {
-  $("#team").text("Loading...");
-
-  $.ajax({
-    url: "team.json",
-    type: "GET",
-    dataType: "json",
-    beforeSend: function() {
-      $("#team").text("Loading...");
-    },
-    success: function(data) {
-      $("#team").empty();
-      $.each(data, function(index, item) {
-        var name = item.name;
-        var position = item.position;
-        var bio = item.bio;
-
-        var teamDiv = $("<div>").appendTo("#team");
-        $("<h2>").text(name).appendTo(teamDiv);
-        $("<h5>").text(position).appendTo(teamDiv);
-        $("<p>").text(bio).appendTo(teamDiv);
-      });
-    },
-    error: function() {
-      $("#team").text("Error retrieving content.");
-    }
-  });
-}
 
 $(document).ready(function() {
-  getDataWithAjax(); // or getDataWithGetJSON();
+  // Call one of the methods to fetch and display data
+  //fetchDataWithGetJSON(); // Uncomment this line to use $.getJSON method
+  // OR
+  fetchDataWithAjax(); // Uncomment this line to use $.ajax method  
 });
-
