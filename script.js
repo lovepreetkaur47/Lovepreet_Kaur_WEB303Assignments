1458899028b1dc225fd4a94d1b22583ecc1f4df0
w0832389 <W0824592@myscc.ca>
2023-11-17 12:52:23 -0500

    to do

--- script.js
+++ script.js
@@ -1,39 +1,119 @@
 $(document).ready(function() {
-    // Your JSON data
-    var characters = [
-        // Your character data here...
-    ];
-
-    // Function to render the table rows
-    function renderTable() {
-        var tbody = $('#characterBody');
-        tbody.empty(); // Clear existing rows
-
-        // Loop through characters and append rows to the table
-        characters.forEach(function(character) {
-            var row = $('<tr>');
-            row.append('<td>' + character.first_name + '</td>');
-            row.append('<td>' + character.last_name + '</td>');
-            row.append('<td>' + character.birth_date + '</td>');
-            row.append('<td>' + character.age + '</td>');
-            row.append('<td>' + character.from + '</td>');
-            row.append('<td>' + character.profession + '</td>');
-
-            tbody.append(row);
-        });
-    }
-
-    // Initial rendering of the table
-    renderTable();
-
-    // Event handler for sorting table on header click
-    $('th a').on('click', function() {
-        var column = $(this).data('sort');
-        characters.sort(function(a, b) {
-            // Assuming all fields are strings, adjust comparison as needed
-            return a[column].localeCompare(b[column]);
-        });
-
-        renderTable();
+    // Load JSON data using AJAX
+    $.ajax({
+      url: 'characters.json',
+      dataType: 'json',
+      success: function(data) {
+        // Call a function to build the table with the JSON data
+        buildTable(data);
+      },
+      error: function(error) {
+        console.log('Error loading JSON data:', error);
+      }
     });
-});
+  
+    function buildTable(data) {
+      // Cache references to table body and table rows
+      var $tableBody = $('#characterBody');
+      var $rows = [];
+  
+      // Iterate through each character in the JSON data
+      $(document).ready(function() {
+        // Load JSON data using AJAX
+        $.ajax({
+          url: 'characters.json',
+          dataType: 'json',
+          success: function(data) {
+            // Call a function to build the table with the JSON data
+            buildTable(data);
+          },
+          error: function(error) {
+            console.log('Error loading JSON data:', error);
+          }
+        });
+      
+        function buildTable(data) {
+          // Cache references to table body and table rows
+          var $tableBody = $('#characterBody');
+          var $rows = [];
+      
+          // Iterate through each character in the JSON data
+          $.each(data, function(index, character) {
+            // Create a table row and append it to the rows array
+            var $row = $('<tr>');
+            $row.append('<td>' + character.first_name + '</td>');
+            $row.append('<td>' + character.last_name + '</td>');
+            $row.append('<td>' + character.birth_date + '</td>');
+            $row.append('<td>' + character.age + '</td>');
+            $row.append('<td>' + character.from + '</td>');
+            $row.append('<td>' + character.profession + '</td>');
+            $rows.push($row);
+          });
+      
+          // Append the rows to the table body
+          $tableBody.append($rows);
+      
+          // Attach click event to table header anchors
+          $('thead a').on('click', function(e) {
+            e.preventDefault();
+      
+            var $anchor = $(this);
+            var sortKey = $anchor.data('sort');
+            var sortOrder = $anchor.data('order') || 'asc';
+      
+            // Update sort order
+            sortOrder = sortOrder === 'asc' ? 'desc' : 'asc';
+            $anchor.data('order', sortOrder);
+      
+            // Sort rows based on the selected column and order
+            $rows.sort(function(rowA, rowB) {
+              var valueA = $(rowA).find('td[data-key=' + sortKey + ']').text();
+              var valueB = $(rowB).find('td[data-key=' + sortKey + ']').text();
+      
+              if (sortOrder === 'asc') {
+                return valueA.localeCompare(valueB);
+              } else {
+                return valueB.localeCompare(valueA);
+              }
+            });
+      
+            // Append the sorted rows to the table body
+            $tableBody.html($rows);
+          });
+        }
+      });
+      
+  
+      // Append the rows to the table body
+      $tableBody.append($rows);
+  
+      // Attach click event to table header anchors
+      $('thead a').on('click', function(e) {
+        e.preventDefault();
+  
+        var $anchor = $(this);
+        var sortKey = $anchor.data('sort');
+        var sortOrder = $anchor.data('order') || 'asc';
+  
+        // Update sort order
+        sortOrder = sortOrder === 'asc' ? 'desc' : 'asc';
+        $anchor.data('order', sortOrder);
+  
+        // Sort rows based on the selected column and order
+        $rows.sort(function(rowA, rowB) {
+          var valueA = $(rowA).find('td[data-key=' + sortKey + ']').text();
+          var valueB = $(rowB).find('td[data-key=' + sortKey + ']').text();
+  
+          if (sortOrder === 'asc') {
+            return valueA.localeCompare(valueB);
+          } else {
+            return valueB.localeCompare(valueA);
+          }
+        });
+  
+        // Append the sorted rows to the table body
+        $tableBody.html($rows);
+      });
+    }
+  });
+  
